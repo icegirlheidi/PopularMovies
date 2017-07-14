c37601ae722389bcc8552288f14ef70d667bdf7c
Yi Wang <icegirlheidi@gmail.com>
2017-07-14 16:23:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/popularmovies/MoviesActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.popularmovies;
+
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.action_settings:
+                Intent intent = new Intent(MoviesActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
