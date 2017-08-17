1db1852b8f8d5085c981352f328e1a6a4b4db939
Yi Wang <icegirlheidi@gmail.com>
2017-08-17 21:35:34 +0300

    Refactor layout using ConstraintLayout

---
+++ app/src/main/java/com/example/android/popularmovies/Video.java
@@ -0,0 +1,74 @@
+package com.example.android.popularmovies;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Video implements Parcelable {
+
+    private String mId;
+    private String mKey;
+    private String mSite;
+    private String mType;
+
+    public Video(String id, String key) {
+        this.mId = id;
+        this.mKey = key;
+    }
+
+    protected Video(Parcel in) {
+        mId = in.readString();
+        mKey = in.readString();
+        mSite = in.readString();
+        mType = in.readString();
+    }
+
+    public static final Creator<Video> CREATOR = new Creator<Video>() {
+        @Override
+        public Video createFromParcel(Parcel in) {
+            return new Video(in);
+        }
+
+        @Override
+        public Video[] newArray(int size) {
+            return new Video[size];
+        }
+    };
+
+    public void setSite(String site) {
+        this.mSite = site;
+    }
+
+    public void setType(String type) {
+        this.mType = type;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getKey() {
+        return mKey;
+    }
+
+    public String getSite() {
+        return mSite;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(mId);
+        parcel.writeString(mKey);
+        parcel.writeString(mSite);
+        parcel.writeString(mType);
+    }
+}
